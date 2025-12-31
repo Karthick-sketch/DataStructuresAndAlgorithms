42f85ab076ccc79ea0af30d462abef8b412c6162
Karthick-sketch <karthick246.kh@outlook.com>
2025-12-31 18:57:11 +0000

    Implement rehashing

--- src/datastructures/nonlinear/hash/HashTable.java
+++ src/datastructures/nonlinear/hash/HashTable.java
@@ -8,12 +8,17 @@ public class HashTable<V> implements Map<Integer, V> {
   private final int RANGE = 11;
   private final ArrayList<Entry<Integer, V>> hashTable;
   private int length = RANGE;
+  private int elements = 0;
 
   public HashTable() {
     hashTable = new ArrayList<>();
     for (int i = 0; i < length; i++) {
       hashTable.add(null);
     }
+  }
+
+  public int size() {
+    return elements;
   }
 
   @Override
@@ -29,6 +34,8 @@ public class HashTable<V> implements Map<Integer, V> {
       }
       current.setNext(entry);
     }
+    elements++;
+    checkRehash();
   }
 
   private V fetch(Integer key) {
@@ -94,6 +101,7 @@ public class HashTable<V> implements Map<Integer, V> {
         } else {
           previous.setNext(current.getNext());
         }
+        elements--;
         return current.getValue();
       }
       current = current.getNext();
@@ -103,5 +111,32 @@ public class HashTable<V> implements Map<Integer, V> {
 
   private int hashDivision(Integer key) {
     return key % length;
+  }
+
+  private void checkRehash() {
+    if (loadFactor() > 0.7) {
+      rehash();
+    }
+  }
+
+  private double loadFactor() {
+    return elements / length;
+  }
+
+  private void rehash() {
+    List<Entry<Integer, V>> entries = new ArrayList<>();
+    for (int i = 0; i < length; i++) {
+      Entry<Integer, V> entry = hashTable.get(i);
+      if (entry != null) {
+        entries.add(entry);
+        entry = entry.getNext();
+      }
+    }
+    hashTable.clear();
+    length += RANGE;
+    for (int i = 0; i < entries.size(); i++) {
+      Entry<Integer, V> entry = entries.get(i);
+      put(entry.getKey(), entry.getValue());
+    }
   }
 }
