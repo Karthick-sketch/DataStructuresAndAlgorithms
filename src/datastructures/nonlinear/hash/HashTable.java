30b4315461bd6ca42d56ef2c3ffb69cc06b66218
Karthick-sketch <karthick246.kh@outlook.com>
2026-01-03 07:47:18 +0000

    Rehash hashtable size x 2 to nearest prime number

--- src/datastructures/nonlinear/hash/HashTable.java
+++ src/datastructures/nonlinear/hash/HashTable.java
@@ -6,8 +6,9 @@ import datastructures.linear.list.List;
 
 public class HashTable<V> implements Map<Integer, V> {
 
-  private final int RANGE = 11;
   private final ArrayList<List<Entry<Integer, V>>> hashTable;
+  private final int RANGE = 11;
+
   private int length = RANGE;
   private int elements = 0;
 
@@ -32,17 +33,19 @@ public class HashTable<V> implements Map<Integer, V> {
     if (entry != null) {
       entry.setValue(value);
     } else {
-      int index = hashDivision(key);
-      List<Entry<Integer, V>> list = hashTable.get(index);
+      List<Entry<Integer, V>> list = getList(key);
       list.add(new Entry<>(key, value));
       elements++;
       checkRehash();
     }
   }
 
+  private List<Entry<Integer, V>> getList(Integer key) {
+    return hashTable.get(hashFunction(key));
+  }
+
   private Entry<Integer, V> fetch(Integer key) {
-    int index = hashDivision(key);
-    List<Entry<Integer, V>> list = hashTable.get(index);
+    List<Entry<Integer, V>> list = getList(key);
     for (int i = 0; i < list.size(); i++) {
       Entry<Integer, V> entry = list.get(i);
       if (entry.getKey().equals(key)) {
@@ -96,8 +99,7 @@ public class HashTable<V> implements Map<Integer, V> {
     if (entry == null) {
       throw new RuntimeException("key not found");
     }
-    int index = hashDivision(key);
-    List<Entry<Integer, V>> list = hashTable.get(index);
+    List<Entry<Integer, V>> list = getList(key);
     list.remove(entry);
     elements--;
     return entry.getValue();
@@ -105,24 +107,36 @@ public class HashTable<V> implements Map<Integer, V> {
 
   @Override
   public void clear() {
+    clear(RANGE);
+  }
+
+  private void clear(int length) {
+    this.length = length;
     elements = 0;
-    length = RANGE;
     hashTable.clear();
     setHashTable();
   }
 
-  private int hashDivision(Integer key) {
-    return key % length;
+  private int hashFunction(Integer key) {
+    return compressionMap(hashCode(key));
   }
 
-  private void checkRehash() {
-    if (loadFactor() > 0.7f) {
-      rehash();
-    }
+  private int hashCode(Integer key) {
+    return key;
+  }
+
+  private int compressionMap(Integer code) {
+    return code % length;
   }
 
   private float loadFactor() {
     return (float) elements / length;
+  }
+
+  private void checkRehash() {
+    if (loadFactor() > 1.0f) {
+      rehash();
+    }
   }
 
   private void rehash() {
@@ -133,13 +147,31 @@ public class HashTable<V> implements Map<Integer, V> {
         entries.add(list.get(j));
       }
     }
-    elements = 0;
-    length += RANGE;
-    hashTable.clear();
-    setHashTable();
+    clear(nearestPrimeNumber(length));
     for (int i = 0; i < entries.size(); i++) {
       Entry<Integer, V> entry = entries.get(i);
       put(entry.getKey(), entry.getValue());
     }
+  }
+
+  private int nearestPrimeNumber(int seed) {
+    int len = seed * 2;
+    for (int i = len, j = len; j > seed; i++, j--) {
+      if (isPrimeNumber(i)) {
+        return i;
+      } else if (isPrimeNumber(j)) {
+        return j;
+      }
+    }
+    return len;
+  }
+
+  private boolean isPrimeNumber(int number) {
+    for (int i = 2; i < number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
