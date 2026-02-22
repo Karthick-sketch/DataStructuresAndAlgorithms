d34677029e3fe6fa841d6f13de4a0a8f7741b3a6
Karthick-sketch <karthick246.kh@outlook.com>
2026-02-22 06:58:00 +0000

    Implement ArrayList

---
+++ src/datastructures/linear/list/ArrayList.java
@@ -0,0 +1,150 @@
+package datastructures.linear.list;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class ArrayList<L> implements List<L> {
+
+  private final int RANGE = 10;
+
+  private Object[] array;
+  private int length = RANGE;
+  private int position = 0;
+
+  public ArrayList() {
+    array = new Object[length];
+  }
+
+  public ArrayList(L[] values) {
+    length = values.length;
+    position = values.length;
+    array = Arrays.copyOf(values, length);
+  }
+
+  @Override
+  public int size() {
+    return position;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return position == 0;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public L get(int index) {
+    validateIndex(index);
+    return (L) array[index];
+  }
+
+  @Override
+  public void add(L value) {
+    array[position] = (Object) value;
+    position++;
+    if (position + 1 >= length) {
+      extend();
+    }
+  }
+
+  @Override
+  public void addAll(L[] values) {
+    for (L value : values) {
+      add(value);
+    }
+  }
+
+  @Override
+  public void set(L value, int index) {
+    validateIndex(index);
+    array[index] = value;
+  }
+
+  @Override
+  public void insert(L value, int index) {
+    for (int i = position; i > index; i--) {
+      array[i] = array[i - 1];
+    }
+    array[index] = value;
+    position++;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public L remove(int index) {
+    validateIndex(index);
+    L value = (L) array[index];
+    for (int i = index; i < position; i++) {
+      array[i] = array[i + 1];
+    }
+    array[position] = null;
+    position--;
+    if (position > RANGE && position < length - RANGE) {
+      shrink();
+    }
+    return value;
+  }
+
+  @Override
+  public L remove(L value) {
+    int index = -1;
+    for (int i = 0; i < position; i++) {
+      if (array[i].equals(value)) {
+        index = i;
+        break;
+      }
+    }
+    return remove(index);
+  }
+
+  @Override
+  public void clear() {
+    position = 0;
+    length = RANGE;
+    array = new Object[length];
+  }
+
+  @Override
+  public String toString() {
+    if (position == 0) {
+      return "[]";
+    }
+    StringBuilder values = new StringBuilder("[");
+    for (int i = 0; i < position - 1; i++) {
+      values.append(array[i]).append(", ");
+    }
+    return values.toString() + array[position - 1] + "]";
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public void forEach(Consumer<L> action) {
+    for (int i = 0; i < position; i++) {
+      action.accept((L) array[i]);
+    }
+  }
+
+  private void validateIndex(int index) {
+    if (index < 0 && index >= position) {
+      throw new IndexOutOfBoundsException();
+    }
+  }
+
+  private void extend() {
+    length += RANGE;
+    adjust();
+  }
+
+  private void shrink() {
+    length -= RANGE;
+    adjust();
+  }
+
+  private void adjust() {
+    Object[] objects = new Object[length];
+    for (int i = 0; i < position; i++) {
+      objects[i] = array[i];
+    }
+    array = objects;
+  }
+}
--- src/datastructures/linear/list/LinkedList.java
+++ src/datastructures/linear/list/LinkedList.java
@@ -1,11 +1,19 @@
-package datastructures.linear.linkedlist;
+package datastructures.linear.list;
 
 import datastructures.linear.node.Node;
+import java.util.function.Consumer;
 
-public class SinglyLinkedList<L> implements LinkedList<L> {
+public class LinkedList<L> implements List<L> {
 
   private Node<L> head;
+  private Node<L> tail;
   private int length;
+
+  public LinkedList() {}
+
+  public LinkedList(L[] values) {
+    addAll(values);
+  }
 
   @Override
   public int size() {
@@ -30,12 +38,9 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
     if (head == null) {
       head = node;
     } else {
-      Node<L> temp = head;
-      while (temp.getNext() != null) {
-        temp = temp.getNext();
-      }
-      temp.setNext(node);
+      tail.setNext(node);
     }
+    tail = node;
     length++;
   }
 
@@ -47,12 +52,28 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
   }
 
   @Override
+  public void set(L value, int index) {
+    if (index >= 0 && index < length) {
+      Node<L> temp = head;
+      for (int i = 1; i <= index; i++) {
+        temp = temp.getNext();
+      }
+      temp.setValue(value);
+    } else {
+      throw new IndexOutOfBoundsException(index);
+    }
+  }
+
+  @Override
   public void insert(L value, int index) {
     if (index >= 0 && index <= length) {
       Node<L> node = new Node<>(value);
       if (index == 0) {
         node.setNext(head);
         head = node;
+      } else if (index == length) {
+        tail.setNext(node);
+        tail = node;
       } else {
         Node<L> temp = head;
         for (int i = 1; i < index; i++) {
@@ -68,24 +89,14 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
   }
 
   @Override
-  public void update(L value, int index) {
-    if (index >= 0 && index < length) {
-      Node<L> temp = head;
-      for (int i = 1; i <= index; i++) {
-        temp = temp.getNext();
-      }
-      temp.setValue(value);
-    } else {
-      throw new IndexOutOfBoundsException(index);
-    }
-  }
-
-  @Override
   public L remove(int index) {
     if (index >= 0 && index < length) {
       Node<L> node = head;
       if (index == 0) {
         head = head.getNext();
+        if (head == null) {
+          tail = null;
+        }
       } else {
         Node<L> temp = head;
         for (int i = 1; i < index; i++) {
@@ -93,6 +104,9 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
         }
         node = temp.getNext();
         temp.setNext(node.getNext());
+        if (node == tail) {
+          tail = temp;
+        }
       }
       length--;
       return node.getValue();
@@ -102,7 +116,8 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
 
   @Override
   public L remove(L value) {
-    Node<L> current = head, previous = null;
+    Node<L> current = head,
+      previous = null;
     while (current != null) {
       if (current.getValue().equals(value)) {
         L obj = current.getValue();
@@ -110,6 +125,9 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
           head = head.getNext();
         } else {
           previous.setNext(current.getNext());
+        }
+        if (current == tail) {
+          tail = previous;
         }
         length--;
         return obj;
@@ -137,6 +155,21 @@ public class SinglyLinkedList<L> implements LinkedList<L> {
   @Override
   public void clear() {
     head = null;
+    tail = null;
     length = 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return length == 0;
+  }
+
+  @Override
+  public void forEach(Consumer<L> action) {
+    Node<L> current = head;
+    while (current != null) {
+      action.accept(current.getValue());
+      current = current.getNext();
+    }
   }
 }
